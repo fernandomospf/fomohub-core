cb8f9f482361099a742276960530c4ae3d4cb1cb
Fernando Mos <fernandomosp@gmail.com>
2026-01-16 19:41:01 -0300

    feat: Enhance CORS configuration with additional origins, methods, and headers, and update the server startup log message.

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,24 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3003;
+
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: [
       'http://localhost:3000',
       'http://localhost:5173',
+      'https://fomo-core.onrender.com',
+      'https://fomo.app'
     ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+
   await app.listen(PORT);
-  console.log(`Application is running on: http://localhost:${PORT}`);
+
+  console.log(`🚀 API running on port ${PORT}`);
 }
+
 bootstrap();
