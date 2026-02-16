6d9ddfb9ce93d1eb50b7d84700e3a45df931ddf1
Fernando Mós <fernandomosp@gmail.com>
2026-02-16 15:36:00 -0300

    feat: add `class-validator` decorators to `AddSetToSessionDto` properties for validation.

--- src/workout-plans/dto/addSetToSession.dto.ts
+++ src/workout-plans/dto/addSetToSession.dto.ts
@@ -1,7 +1,19 @@
+import { IsUUID, IsNumber, IsOptional } from 'class-validator';
+
 export class AddSetToSessionDto {
+  @IsUUID()
   workout_exercise_id: string;
+
+  @IsNumber()
   set_number: number;
+
+  @IsNumber()
   reps: number;
+
+  @IsNumber()
   weight: number;
+
+  @IsOptional()
+  @IsNumber()
   rest_seconds?: number;
 }
